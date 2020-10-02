b87337a9e952ee041df2747a66a4e6289e9eabd6
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-02 17:25:14 -0400

    'fixedconfig'

--- modules/app/app.js
+++ modules/app/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('../../config')
 
 const app = express()
 
